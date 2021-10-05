507a4d88d5d1d9ca583b0ce98f08c5521914a865
antonia-had <ah986@cornell.edu>
2021-10-05 17:00:57 -0400

    output extraction job

---
+++ scripts/submit_data_extraction.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --cpus-per-task=1
+#SBATCH -p normal
+#SBATCH -t 00:10:00
+#SBATCH --export=ALL
+#SBATCH --mail-user=ah986@cornell.edu
+#SBATCH --mail-type=ALL
+#SBATCH --array=1-10         # array of tasks to execute
+
+source /home/fs02/pmr82_0001/ah986/statemod_training/bin/activate
+DIR="/outputs/"
+if not [ -d "$DIR" ]; then
+  mkdir $DIR
+fi
+srun python3 ./scripts/extract_xdd.py $SLURM_ARRAY_TASK_ID
